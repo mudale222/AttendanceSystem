2369a038ec5330ec3b71c7fb8b5f92c6bedf108b
mudale222 <yuziel@walla.co.il>
2021-01-11 11:12:05 +0200

    git init

---
+++ dxcEx/LoginForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace dxcEx {
+    partial class LoginForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.TimerLabel = new System.Windows.Forms.Label();
+            this.LogInLabel = new System.Windows.Forms.Label();
+            this.LogOutLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(43, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name:";
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(152, 24);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userNameTextBox.TabIndex = 1;
+            this.userNameTextBox.TextChanged += new System.EventHandler(this.userNameTextBoxChanged);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(152, 65);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.passwordTextBoxChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.NavajoWhite;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(43, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(118, 111);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(60, 23);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "LOGIN";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButtonClicked);
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(192, 111);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(60, 23);
+            this.logoutButton.TabIndex = 5;
+            this.logoutButton.Text = "LOGOUT";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButtonClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(656, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "User Offline!";
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.Location = new System.Drawing.Point(657, 100);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(0, 13);
+            this.TimerLabel.TabIndex = 8;
+            // 
+            // LogInLabel
+            // 
+            this.LogInLabel.AutoSize = true;
+            this.LogInLabel.Location = new System.Drawing.Point(657, 65);
+            this.LogInLabel.Name = "LogInLabel";
+            this.LogInLabel.Size = new System.Drawing.Size(0, 13);
+            this.LogInLabel.TabIndex = 9;
+            // 
+            // LogOutLabel
+            // 
+            this.LogOutLabel.AutoSize = true;
+            this.LogOutLabel.Location = new System.Drawing.Point(657, 133);
+            this.LogOutLabel.Name = "LogOutLabel";
+            this.LogOutLabel.Size = new System.Drawing.Size(0, 13);
+            this.LogOutLabel.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(577, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Login Time:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label5.Location = new System.Drawing.Point(577, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Time Passed:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label6.Location = new System.Drawing.Point(577, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "LogOutTime:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "SIGNUP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.signupButtonClicked);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LogOutLabel);
+            this.Controls.Add(this.LogInLabel);
+            this.Controls.Add(this.TimerLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label TimerLabel;
+        private System.Windows.Forms.Label LogInLabel;
+        private System.Windows.Forms.Label LogOutLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ dxcEx/LoginForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dxcEx {
+    public partial class LoginForm : Form {
+        System.Windows.Forms.Timer userTimer = null;
+        //TimeSpan userOnlineTime = new TimeSpan();
+        DateTime userConnectedTime = new DateTime();
+        const string MyConString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\mudale\\source\\repos\\dxcEx\\dxcEx\\MainDb.mdf;Integrated Security=True;";
+
+
+        public LoginForm() {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e) {
+
+        }
+        private void StartTimer() {
+            userTimer = new System.Windows.Forms.Timer();
+            userTimer.Interval = 1000;
+            userTimer.Tick += new EventHandler(t_Tick);
+            userTimer.Enabled = true;
+            userConnectedTime = DateTime.Now;
+        }
+
+        void t_Tick(object sender, EventArgs e) {
+            var time_span = (DateTime.Now - userConnectedTime);
+            var hh = time_span.Hours;
+            var mm = time_span.Minutes;
+            var ss = time_span.Seconds;
+
+            TimerLabel.Text = string.Format("{0}:{1}:{2}", hh, mm, ss);
+        }
+
+        private void loginButtonClicked(object sender, EventArgs e) {
+            var cmd = new SqlCommand();
+            var userName = userNameTextBox.Text;
+            var userHashSalt = new HashSalt();
+
+            using (SqlConnection connection = new SqlConnection(MyConString)) {
+                connection.Open();
+
+                cmd.Parameters.AddWithValue("@userName", userName);
+                cmd.CommandText = "SELECT TOP 1 * FROM Users WHERE Username=@userName"; 
+                cmd.Connection = connection;
+                using (SqlDataReader oReader = cmd.ExecuteReader()) {
+                    if (oReader.Read())
+                        userHashSalt = new HashSalt { Hash = oReader["Hash"].ToString(), Salt = oReader["Salt"].ToString() };
+                    else {
+                        MessageBox.Show("User or password invalid (testing: no user)");
+                        return;
+                    }
+                }
+
+                connection.Close();
+            }
+
+
+            bool isPasswordMatched = HashSalt.VerifyPassword(passwordTextBox.Text, userHashSalt.Hash, userHashSalt.Salt);
+
+            if (isPasswordMatched) {
+                MessageBox.Show("User or password invalid (testing: Password match!)");
+            }
+            else {
+                MessageBox.Show("User or password invalid (testing:Password invalid!)");
+                return;
+            }
+
+            LogInLabel.Text = DateTime.Now.ToString();
+            StartTimer();
+        }
+
+        private void logoutButtonClicked(object sender, EventArgs e) {
+            LogOutLabel.Text = DateTime.Now.ToString();
+            userTimer.Enabled = false;
+        }
+
+        private void signupButtonClicked(object sender, EventArgs e) {
+            var userName = userNameTextBox.Text;
+            var password = passwordTextBox.Text;
+            var cmd = new SqlCommand();
+            if (userName.Length < 6 || password.Length < 6 /*use premade libary for this*/) {
+                MessageBox.Show("Need at lease 1 number and  blablalba etc...");
+                return;
+            }
+
+            HashSalt hashSalt = HashSalt.GenerateSaltedHash(64, password);
+
+            using (SqlConnection connection = new SqlConnection(MyConString)) {
+                connection.Open();
+
+                cmd.Parameters.AddWithValue("@userName", userName);
+                cmd.Parameters.AddWithValue("@hash", hashSalt.Hash);
+                cmd.Parameters.AddWithValue("@salt", hashSalt.Salt);
+                cmd.CommandText = "INSERT INTO Users(Username ,Hash, Salt) VALUES ( @userName, @hash, @salt)";
+                cmd.Connection = connection;
+                try {
+                    cmd.ExecuteNonQuery();
+                } catch (SqlException ex) {
+                    if (ex.Number == 2601 || ex.Number == 2627) {
+                        MessageBox.Show("User already exist!");
+                        connection.Close();
+                        return;
+                    }
+                }
+
+                MessageBox.Show("New user added!");
+                connection.Close();
+            }
+        }
+
+        private void userNameTextBoxChanged(object sender, EventArgs e) {
+
+        }
+
+        private void passwordTextBoxChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
